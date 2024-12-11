2c8a471ea2df4da00efdc910de3aa275c3f46796
Laura Fiona <laurapinzonm7@gmail.com>
2024-12-11 16:58:49 -0500

    Promise all

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -28,19 +28,48 @@ export const promiseComponent = (element) => {
     const id1 = '5d86371fd55e2e2a30fe1cc3';
     const id2 = '5d86371fd55e2e2a30fe1cc4';
 
-    let hero1, hero2;
+  Promise.all([
+    findHero(id1),
+    findHero(id2),
+  ])
+  .then(([hero1, hero2])=>renderTwoHeroes(hero1,hero2))
+  .catch(renderError);
 
-    findHero (id1)
-        .then((hero1) => {
-          
-            findHero (id2)
-            .then ((hero2)=>{
-                renderTwoHeroes(hero1,hero2)
-            })
-        })
-        .catch(renderError);
-  
+
+
+
+
+
 }
+
+
+//! Forma 2
+// let hero1;
+//  findHero (id1)
+//     .then(hero =>{
+//         hero1 = hero;
+//         return findHero(id2);
+//     }).then (hero2 =>{
+//         renderTwoHeroes(hero1,hero2);
+//     })
+//     .catch (renderError);
+// }
+
+
+
+
+//! Forma 1
+//     findHero (id1)
+//         .then((hero1) => {
+          
+//             findHero (id2)
+//             .then ((hero2)=>{
+//                 renderTwoHeroes(hero1,hero2)
+//             })
+//         })
+//         .catch(renderError);
+  
+// }
 
 
 /**
