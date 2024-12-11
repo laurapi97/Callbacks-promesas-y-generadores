aed3a5654502c42e405223de0ac3c0f8d715c9da
Laura Fiona <laurapinzonm7@gmail.com>
2024-12-11 17:50:40 -0500

    promise race

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -16,6 +16,7 @@ export const callbacksComponent = (element) => {
         }
     
         findHero(id2, (error, valorHero2) => {
+            
             if (error){
                 element.innerHTML= error ;
                 return;
